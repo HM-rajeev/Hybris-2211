e58c18fa524720ce61f54fdc5006bd0f7bf2bc0b
Rajeev Kumar <rajeev.kumar2@happiestminds.com>
2023-07-22 11:56:56 +0530

    Product cat

--- custom/hm/hminitialdata/src/com/hm/initialdata/setup/InitialDataSystemSetup.java
+++ custom/hm/hminitialdata/src/com/hm/initialdata/setup/InitialDataSystemSetup.java
@@ -12,12 +12,13 @@ import de.hybris.platform.core.initialization.SystemSetup.Type;
 import de.hybris.platform.core.initialization.SystemSetupContext;
 import de.hybris.platform.core.initialization.SystemSetupParameter;
 import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
-import com.hm.initialdata.constants.HmInitialDataConstants;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Required;
+
+import com.hm.initialdata.constants.HmInitialDataConstants;
 
 
 /**
@@ -29,6 +30,10 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	private static final String IMPORT_CORE_DATA = "importCoreData";
 	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
 	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+	private static final String IMPORT_HM_PRODUCT_CATALOG = "importhmProductCatalog";
+	private static final String IMPORT_HM_PRODUCT_CATALOG_MULTI_D = "importhmProductCatalogmultid";
+	private static final String IMPORT_HM_PRODUCT_CATALOG_SAMPLE_DATA = "importhmProductCatalogsampledata";
+
 
 	private CoreDataImportService coreDataImportService;
 	private SampleDataImportService sampleDataImportService;
@@ -45,22 +50,28 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 		params.add(createBooleanSystemSetupParameter(IMPORT_CORE_DATA, "Import Core Data", true));
 		params.add(createBooleanSystemSetupParameter(IMPORT_SAMPLE_DATA, "Import Sample Data", true));
 		params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
+		params.add(createBooleanSystemSetupParameter(IMPORT_HM_PRODUCT_CATALOG, "import hm Product Catalog", true));
+		params.add(createBooleanSystemSetupParameter(IMPORT_HM_PRODUCT_CATALOG_MULTI_D, "import hm Product Catalog multi d", true));
+		params.add(createBooleanSystemSetupParameter(IMPORT_HM_PRODUCT_CATALOG_SAMPLE_DATA, "import hm Product Catalog Sample Data",
+				true));
 		// Add more Parameters here as you require
 
 		return params;
 	}
 
 	/**
-	 * Implement this method to create initial objects. This method will be called by system creator during
-	 * initialization and system update. Be sure that this method can be called repeatedly.
-	 * 
+	 * Implement this method to create initial objects. This method will be called by system creator during initialization
+	 * and system update. Be sure that this method can be called repeatedly.
+	 *
 	 * @param context
-	 *           the context provides the selected parameters and values
+	 *                   the context provides the selected parameters and values
 	 */
 	@SystemSetup(type = Type.ESSENTIAL, process = Process.ALL)
 	public void createEssentialData(final SystemSetupContext context)
 	{
 		// Add Essential Data here as you require
+
+
 	}
 
 	/**
@@ -85,7 +96,7 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	 * </pre>
 	 *
 	 * @param context
-	 *           the context provides the selected parameters and values
+	 *                   the context provides the selected parameters and values
 	 */
 	@SystemSetup(type = Type.PROJECT, process = Process.ALL)
 	public void createProjectData(final SystemSetupContext context)
@@ -93,6 +104,71 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 		/*
 		 * Add import data for each site you have configured
 		 */
+		if (this.getBooleanSystemSetupParameter(context, IMPORT_HM_PRODUCT_CATALOG))
+		{
+			importImpexFile(context, "/hmstorefront/import/coredata/productCatalogs/hmProductCatalog/catalog.impex");
+			importImpexFile(context, "/hmstorefront/import/coredata/productCatalogs/hmProductCatalog/catalog_en.impex");
+			importImpexFile(context, "/hmstorefront/import/coredata/productCatalogs/hmProductCatalog/catalog_de.impex");
+		}
+
+		if (this.getBooleanSystemSetupParameter(context, IMPORT_HM_PRODUCT_CATALOG_MULTI_D))
+		{
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products_en.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products_de.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-classifications.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-classifications_en.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-media.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-pos-stocklevels.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-prices.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-stock-levels.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/multi-d/dimension-products-tax.impex");
+
+		}
+
+		if (this.getBooleanSystemSetupParameter(context, IMPORT_HM_PRODUCT_CATALOG_SAMPLE_DATA))
+		{
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories_en.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories_de.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories-classifications.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories-classifications_en.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories-classifications_de.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/categories-media.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/classifications-units.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products_en.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products_de.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-classifications.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-classifications_en.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-futurestock.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-media.impex");
+			importImpexFile(context,
+					"/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-pos-stocklevels.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-prices.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-relations.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-stocklevels.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-tax.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/suppliers.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/suppliers_en.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/suppliers_de.impex");
+			importImpexFile(context, "/hmstorefront/import/sampledata/productCatalogs/hmProductCatalog/products-media.impex");
+		}
 	}
 
 	public CoreDataImportService getCoreDataImportService()
