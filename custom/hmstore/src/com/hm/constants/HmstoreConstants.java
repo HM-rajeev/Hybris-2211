04dc173a215f757f5ee9102170461b570ef09054
rajeevpseudo <ur.rajeev@gmail.com>
2023-08-19 22:14:52 +0530

    remove unuanted file

---
+++ custom/hmstore/src/com/hm/constants/HmstoreConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.hm.constants;
+
+/**
+ * Global class for all Hmstore constants. You can add global constants for your extension into this class.
+ */
+public final class HmstoreConstants extends GeneratedHmstoreConstants
+{
+	public static final String EXTENSIONNAME = "hmstore";
+
+	private HmstoreConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ custom/hmstore/src/com/hm/setup/HmStoreSystemSetup.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.hm.setup;
+
+import de.hybris.platform.commerceservices.dataimport.impl.CoreDataImportService;
+import de.hybris.platform.commerceservices.dataimport.impl.SampleDataImportService;
+import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
+import de.hybris.platform.commerceservices.setup.events.SampleDataImportedEvent;
+import de.hybris.platform.core.initialization.SystemSetup;
+import de.hybris.platform.core.initialization.SystemSetupContext;
+import de.hybris.platform.core.initialization.SystemSetupParameter;
+import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Required;
+
+import com.hm.constants.HmstoreConstants;
+
+
+@SystemSetup(extension = HmstoreConstants.EXTENSIONNAME)
+public class HmStoreSystemSetup extends AbstractSystemSetup
+{
+	public static final String HM = "hm";
+	public static final String HM_STANDALONE = "hm-standalone";
+
+	private static final String IMPORT_CORE_DATA = "importCoreData";
+	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
+	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+
+	private CoreDataImportService coreDataImportService;
+	private SampleDataImportService sampleDataImportService;
+
+	@SystemSetupParameterMethod
+	@Override
+	public List<SystemSetupParameter> getInitializationOptions()
+	{
+		final List<SystemSetupParameter> params = new ArrayList<SystemSetupParameter>();
+
+		params.add(createBooleanSystemSetupParameter(IMPORT_CORE_DATA, "Import Core Data", true));
+		params.add(createBooleanSystemSetupParameter(IMPORT_SAMPLE_DATA, "Import Sample Data", true));
+		params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
+
+		return params;
+	}
+
+	/**
+	 * This method will be called during the system initialization.
+	 *
+	 * @param context
+	 *           the context provides the selected parameters and values
+	 */
+	@SystemSetup(type = SystemSetup.Type.PROJECT, process = SystemSetup.Process.ALL)
+	public void createProjectData(final SystemSetupContext context)
+	{
+		final List<ImportData> importData = new ArrayList<ImportData>();
+
+		final ImportData electronicsImportData = new ImportData();
+		electronicsImportData.setProductCatalogName(HM);
+		electronicsImportData.setContentCatalogNames(Arrays.asList(HM));
+		electronicsImportData.setStoreNames(Arrays.asList(HM));
+		electronicsImportData.setSiteNames(Arrays.asList(HM_STANDALONE));
+		importData.add(electronicsImportData);
+
+		getCoreDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+
+		getSampleDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
+	}
+
+	public CoreDataImportService getCoreDataImportService()
+	{
+		return coreDataImportService;
+	}
+
+	@Required
+	public void setCoreDataImportService(final CoreDataImportService coreDataImportService)
+	{
+		this.coreDataImportService = coreDataImportService;
+	}
+
+	public SampleDataImportService getSampleDataImportService()
+	{
+		return sampleDataImportService;
+	}
+
+	@Required
+	public void setSampleDataImportService(final SampleDataImportService sampleDataImportService)
+	{
+		this.sampleDataImportService = sampleDataImportService;
+	}
+}
